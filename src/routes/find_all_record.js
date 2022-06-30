d3a359895a004636ff6869246ae839d324340543
Eddy <andryrabeharilalao@gmail.com>
2022-06-30 21:51:44 +0200

    [ADD]: Put semicolown on find_all_record.js

--- src/routes/find_all_record.js
+++ src/routes/find_all_record.js
@@ -1,23 +1,22 @@
-const {query} = require("express")
-const { Op } = require('sequelize')
-const { db_access } = require('../db/bridge')
-const auth = require('../auth/auth')
+const { Op } = require('sequelize');
+const { db_access } = require('../db/bridge');
+const auth = require('../auth/auth');
 
 module.exports = (app) => {
     app.get('/api/findall/:model', auth, (req, res) => {
-        const model = req.params.model
-        let database = db_access()[model]
+        const model = req.params.model;
+        let database = db_access()[model];
 
         if (database) {
-            const first_name = req.query.first_name
-            const last_name = req.query.last_name
-            const email = req.query.email
-            const limit = parseInt(req.query.limit) || 5
+            const first_name = req.query.first_name;
+            const last_name = req.query.last_name;
+            const email = req.query.email;
+            const limit = parseInt(req.query.limit) || 5;
 
             if (first_name && last_name && email){
                 if (first_name.length < 2 || last_name.length < 2 || email.length < 2) {
-                    const message = 'The term used in search must have at least two character'
-                    return res.status(400).json({message})
+                    const message = 'The term used in search must have at least two character';
+                    return res.status(400).json({message});
                 }
                 return database.findAndCountAll({
                     where: {
@@ -34,15 +33,15 @@ module.exports = (app) => {
                     order: ['last_name'],
                     limit: limit
                 }).then(({count, rows}) => {
-                    const message = `There is ${count} records found with the term ${first_name} ${last_name} - ${email}.`
-                    return res.json({message: message, data: rows})
-                })
+                    const message = `There is ${count} records found with the term ${first_name} ${last_name} - ${email}.`;
+                    return res.json({message: message, data: rows});
+                });
             }
 
             if (first_name && last_name){
                 if (first_name.length < 2 || last_name.length < 2) {
-                    const message = 'The term used in search must have at least two character'
-                    return res.status(400).json({message})
+                    const message = 'The term used in search must have at least two character';
+                    return res.status(400).json({message});
                 }
                 return database.findAndCountAll({
                     where: {
@@ -56,16 +55,16 @@ module.exports = (app) => {
                     order: ['last_name'],
                     limit: limit
                 }).then(({count, rows}) => {
-                    const message = `There is ${count} records found with the term ${first_name} ${last_name}.`
-                    return res.json({message: message, data: rows})
-                })
+                    const message = `There is ${count} records found with the term ${first_name} ${last_name}.`;
+                    return res.json({message: message, data: rows});
+                });
             }
 
             if (first_name || last_name || email){
-                const search_value = first_name || last_name || email
+                const search_value = first_name || last_name || email;
                 if (search_value.length < 2) {
-                    const message = 'The term used in search must have at least two character'
-                    return res.status(400).json({message})
+                    const message = 'The term used in search must have at least two character';
+                    return res.status(400).json({message});
                 }
 
                 if (first_name) {
@@ -77,9 +76,9 @@ module.exports = (app) => {
                         order: ['first_name'],
                         limit: limit
                     }).then(({count, rows}) => {
-                        const message = `There is ${count} records found with the term ${first_name}.`
-                        return res.json({message: message, data: rows})
-                    })
+                        const message = `There is ${count} records found with the term ${first_name}.`;
+                        return res.json({message: message, data: rows});
+                    });
 
                 }
                 else if(last_name){
@@ -91,9 +90,9 @@ module.exports = (app) => {
                         order: ['last_name'],
                         limit: limit
                     }).then(({count, rows}) => {
-                        const message = `There is ${count} records found with the term ${last_name}.`
-                        return res.json({message: message, data: rows})
-                    })
+                        const message = `There is ${count} records found with the term ${last_name}.`;
+                        return res.json({message: message, data: rows});
+                    });
                 }
                 else if (email){
                     return database.findAndCountAll({
@@ -104,26 +103,26 @@ module.exports = (app) => {
                         order: ['email'],
                         limit: limit
                     }).then(({count, rows}) => {
-                        const message = `There is ${count} records found with the email ${email}.`
-                        return res.json({message: message, data: rows})
-                    })
+                        const message = `There is ${count} records found with the email ${email}.`;
+                        return res.json({message: message, data: rows});
+                    });
                 }
             }
             else {
                 database.findAll({order: ['id']}).then(record => {
-                    const message = `The ${model} record list is successfully kept`
-                    res.json({message: message, data: record})
+                    const message = `The ${model} record list is successfully kept`;
+                    res.json({message: message, data: record});
                 }).catch(error => {
-                    const message = `The ${model} record cannot be get, Please try after a while.`
-                    res.status(500).json({message: message, data: error })
-                })
+                    const message = `The ${model} record cannot be get, Please try after a while.`;
+                    res.status(500).json({message: message, data: error });
+                });
             }
         }
         else{
             const message = `An error occurred during the operation, please try again later. \n
-            Posted URL: api/findall/${model}`
-            res.status(500).json({ message })
+            Posted URL: api/findall/${model}`;
+            res.status(500).json({ message });
         }
 
-    })
-}
+    });
+};
